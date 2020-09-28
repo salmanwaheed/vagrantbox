5e8a438a13f72883dffbc27125693cb64f89a2b7
Salman Waheed <mkdirenv@gmail.com>
2020-09-29 00:58:37 +0400

    removed docker and docker-compose installations

--- provision.sh
+++ provision.sh
@@ -13,12 +13,6 @@ if [ ! `sed -n "/^$1/p" /etc/passwd` ]; then
   echo "$4" >> /home/$1/.ssh/authorized_keys
   chmod 600 /home/$1/.ssh/authorized_keys
 
-  # config
-  # echo 'Host 192.168.55.66' >> /home/$1/.ssh/config
-  # echo 'StrictHostKeyChecking no' >> /home/$1/.ssh/config
-  # echo 'UserKnownHostsFile /dev/null' >> /home/$1/.ssh/config
-  # chmod 600 /home/$1/.ssh/config
-
   # copy authorized key
   cp -pr /home/$1/.ssh /root
 
@@ -35,19 +29,4 @@ if [ ! `sed -n "/^$1/p" /etc/passwd` ]; then
   # delete existed users
   userdel -rf vagrant >/dev/null 2>&1
   userdel -rf ubuntu >/dev/null 2>&1
-
-  apt-get update
-  apt-get install -y docker.io vim
-  usermod -aG docker $1
-  chown -R $1:$1 /var/run/docker.sock
-
-  curl -sL "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/bin/docker-compose && chmod +x /usr/bin/docker-compose
-
-  VIM_DIR=/home/$1/.vim
-
-  curl -fsSL https://raw.githubusercontent.com/salmanwaheed/terminal-beauty/master/.vim/vimrc -o "$VIM_DIR/vimrc"
-
-  ln -sf "$VIM_DIR/vimrc" "/home/$1/.vimrc"
-
-  chown -R $1:$1 /home/$1
 fi
